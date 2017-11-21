6d9428961859a013417f10592017bf75d0a29ac8
Dennis Pham <dennispham97@hotmail.com>
2017-11-21 13:23:03 -0600

    finished element initialization.

--- main.cpp
+++ main.cpp
@@ -6,11 +6,14 @@
 
 using namespace std;
 
+bool hashing(int data);
+int hashArr[125];
+
+
 int main()
 {
-   int x;
    B_search bsearch;
-  // int array[125];
+   int array[100];
    //initialize array to 126 elements.
    srand(time(NULL));
    //seed random generator based on time.
@@ -20,15 +23,43 @@ int main()
    if(xfiles.is_open())
       cout << "file is open\n";
       
-   for(int i = 0; i < 10; i++)
+   for(int i = 0; i <= 100; i++)
    {
-      x = ((i + 1) * rand())% 101;
-      bsearch.initArr(i, x);
-      cout << "element " << bsearch.returnElement(i) << endl;
+      array[i] = ((i + 1) * rand()%300) + 2300;
+      cout << "element " << array[i] << endl;
    }
-      
-
-      
+   
+   for(int i = 0; i <= 100; i++)
+   {
+      hashing(array[i]);
+   }
+   
+   for(int i = 0; i <= 125; i++)
+   {
+      cout << "hashed arr " << hashArr[i] << endl;
+   }
+   
    xfiles.close();
    return 0;
+}
+
+bool hashing(int data)
+{
+   int hash;
+   hash = data%101;
+   bool letmeoutofhere;
+   while(letmeoutofhere == false)
+   {
+      if(hashArr[hash] != 0)
+      {
+         hashArr[hash] = data;
+         letmeoutofhere = true;
+      }
+      else
+      {
+         hashArr[hash += 3] = data;
+         letmeoutofhere = false;
+      }   
+   }
+   return true;
 }
